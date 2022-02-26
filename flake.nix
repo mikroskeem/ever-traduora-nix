1b3e4c8d183e3ea0d9f185a195c20144995686ac
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-02-26 20:28:16 +0200

    Fix outputs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "eteu-ever-traduora";
+  description = "ever-traduora-nix";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
@@ -7,24 +7,26 @@
   outputs = { self, nixpkgs, flake-utils }:
     let
       supportedSystems = [
-        "aarch64-linux"
         "aarch64-darwin"
-        "x86_64-linux"
+        "aarch64-linux"
         "x86_64-darwin"
+        "x86_64-linux"
       ];
     in
-    flake-utils.lib.eachSystem supportedSystems (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      rec {
-        packages.esbuild-0_14_24 = pkgs.callPackage ./esbuild-0_14_24.nix { };
-        packages.ever-traduora-api = pkgs.callPackage ./default.nix { esbuild = packages.esbuild-0_14_24; };
-        packages.ever-traduora-web = packages.ever-traduora-api.web;
+    flake-utils.lib.eachSystem supportedSystems
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          packages.esbuild-0_14_24 = pkgs.callPackage ./esbuild-0_14_24.nix { };
+          packages.ever-traduora-api = pkgs.callPackage ./default.nix { esbuild = packages.esbuild-0_14_24; };
+          packages.ever-traduora-web = packages.ever-traduora-api.web;
 
-        defaultPackage = packages.ever-traduora-api;
-
-        nixosModules.ever-traduora = import ./module.nix;
-        nixosModule = nixosModules.ever-traduora;
-      });
+          defaultPackage = packages.ever-traduora-api;
+        })
+    // rec {
+      nixosModules.ever-traduora = import ./module.nix;
+      nixosModule = nixosModules.ever-traduora;
+    };
 }
